ef67248129e0ae7fd7b59de6a45f4187def7c6e8
kana <suierkulovkanat23@gmail.com>
2026-02-10 01:57:32 +0500

    Fix static root and requirements

--- core/settings.py
+++ core/settings.py
@@ -2,22 +2,20 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# .env файлын жүктейміз (API кілттер үшін)
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# API кілттерді оқимыз
 GEMINI_KEYS = os.getenv("GEMINI_API_KEYS").split(',') if os.getenv("GEMINI_API_KEYS") else []
 
-SECRET_KEY = 'django-insecure-change-me-please'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-me-please')
 
-# Дебаг режимі қосулы (қателерді көру үшін)
+# Render-де True қалдыра тұрыңыз (қатені көру үшін), кейін False жасаймыз
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# МАҢЫЗДЫ: Жұлдызша қою керек
+ALLOWED_HOSTS = ['*']
 
-# Қосымшалар тізімі
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,11 +23,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testing',  # <--- Біздің қосымша (Мұны ұмытпаңыз!)
+    'testing',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # <--- БҰЛ МІНДЕТТІ ТҮРДЕ КЕРЕК
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -65,18 +64,16 @@ DATABASES = {
     }
 }
 
-# --- МАҢЫЗДЫ ӨЗГЕРІС ---
-# Пароль тексеруді өшірдік. Енді "123" паролін қабылдайды.
 AUTH_PASSWORD_VALIDATORS = []
 
-LANGUAGE_CODE = 'ru-ru' # Сайт тілі (орысша)
-
+LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
+# --- STATIC FILES БАПТАУЛАРЫ (Ең маңызды жері) ---
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # <--- ОСЫ ЖОЛ ЖЕТІСПЕЙ ТҰРДЫ
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
